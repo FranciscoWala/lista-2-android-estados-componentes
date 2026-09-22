c57754bfcc951656c408aca96899b4d8834e3f08
FRANCISCO WALA NORONHA MACEDO <25203678@dsn127m1246728.cfp127aluno.local>
2026-09-22 16:57:16 -0300

    Aprendendo sobre estados com inputs

--- app/src/main/java/com/example/estadoscomponentes/MainActivity.kt
+++ app/src/main/java/com/example/estadoscomponentes/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.estadoscomponentes
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,9 +13,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Label
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,6 +28,8 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,8 +43,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             EstadosComponentesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    BasicComponentsScreen(modifier = Modifier
-                        .padding(innerPadding))
+                    BasicComponentsScreen(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                    )
                 }
             }
         }
@@ -44,9 +54,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BasicComponentsScreen (modifier: Modifier = Modifier){
+fun BasicComponentsScreen(modifier: Modifier = Modifier) {
+    val textoInput = remember {
+        mutableStateOf("")
+    }
+
+    val quantidade = remember {
+        mutableStateOf("")
+    }
+
     Column(
-        modifier= modifier
+        modifier = modifier
             .fillMaxSize()
             .background(Color(100, 236, 147, 255))
     ) {
@@ -59,7 +77,7 @@ fun BasicComponentsScreen (modifier: Modifier = Modifier){
             modifier = Modifier
 //                .background(Color.White)
                 .fillMaxWidth(),
-                textAlign = TextAlign.End,
+            textAlign = TextAlign.End,
             letterSpacing = 4.sp
         )
         Text(
@@ -68,23 +86,46 @@ fun BasicComponentsScreen (modifier: Modifier = Modifier){
             fontWeight = FontWeight.Medium,
             color = Color(0, 0, 0, 255),
             fontFamily = Righteous,
-                modifier= Modifier
-                    .align(Alignment.CenterHorizontally)
-        )
-        Row(
             modifier = Modifier
-
+                .align(Alignment.CenterHorizontally)
+        )
+        Column (
+            modifier = Modifier
         ) {
-            AndroidEnemy(
-                modifier = Modifier.size(100.dp),
-                color = Color.Red
-            )
-            AndroidEnemy(
-                modifier = Modifier.size(100.dp),
-                color = Color.Black
-            )
-        }
 
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = textoInput.value,
+                onValueChange = { novoValor ->
+                    Log.i("teste", novoValor)
+                    textoInput.value = novoValor
+                },
+                keyboardOptions = KeyboardOptions(
+                    capitalization = KeyboardCapitalization.Words
+                ),
+                label = {
+                    Text("Email")
+                }
+            )
+
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = quantidade.value,
+                onValueChange = { novoValor ->
+                    Log.i("teste", novoValor)
+                    quantidade.value = novoValor
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                ),
+                placeholder = {
+                    Text(
+                        text = "Senha"
+                    )
+                }
+            )
+
+        }
     }
 }
 
